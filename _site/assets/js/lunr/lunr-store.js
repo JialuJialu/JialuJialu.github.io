1e0461f6c2368ebe7bcfc0548f68c97a0a1e6191
JialuJialu <baojialuhz@gmail.com>
2021-09-19 19:18:15 -0400

    trying to deleting ~jialu as the baseurl

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Scribe notes on algorithms and complexities",
         "excerpt":"Here are some notes I scribed for courses I take during the first year of my Ph.D. study. These notes have not been scrutinized as peer-reviewed works would have, and I am responsible for any mistakes within it. Continuous Optimization: Gradient Descent. This is a lecture from CS 787 Advanced...","categories": ["notes"],
         "tags": [],
-        "url": "/~jialu/notes/scribes/",
+        "url": "/notes/scribes/",
         "teaser": null
       }]
